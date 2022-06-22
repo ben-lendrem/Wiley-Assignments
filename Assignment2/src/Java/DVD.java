f5675b6da23a6c49d53aa77b604393be97a9dd8c
ben-lendrem <ben.lendrem@gmail.com>
2022-06-22 21:38:50 +0100

    Fixed?
    Sorted the class files out I think?

---
+++ Assignment2/src/Java/DVD.java
@@ -0,0 +1,12 @@
+public class DVD {
+    private String title = null;
+    private int id = -1;
+    private String releaseDate = null; //Using String instead of Date class, might change later if time allows
+    private char ageRating = Character.MIN_VALUE;
+    private String director = null;
+    private String studio = null;
+    private String note = null;
+
+    public DVD() {
+    }
+}
